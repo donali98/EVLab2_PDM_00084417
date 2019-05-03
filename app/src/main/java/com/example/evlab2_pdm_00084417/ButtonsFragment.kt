9505ff71062a6a3e3c697629358b6e62fbb6e512
Donali <00084417@uca.edu.sv>
2019-05-03 07:59:50 -0600

    Click Listener added

--- app/src/main/java/com/example/evlab2_pdm_00084417/ButtonsFragment.kt
+++ app/src/main/java/com/example/evlab2_pdm_00084417/ButtonsFragment.kt
@@ -1,21 +1,35 @@
 package com.example.evlab2_pdm_00084417
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import android.widget.Button
 
 
 class ButtonsFragment : Fragment() {
 
+    lateinit var buttonsHelper: ButtonsHelper
+    lateinit var btnPrev:Button
+
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        buttonsHelper = context as ButtonsHelper
+
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buttons, container, false)
+        val view = inflater.inflate(R.layout.fragment_buttons, container, false)
+        btnPrev = view.findViewById(R.id.btn_prev)
+        btnPrev.setOnClickListener {
+            buttonsHelper.onPrevClickListener()
+        }
+        return view
     }
 
 
---
+++ app/src/main/java/com/example/evlab2_pdm_00084417/ButtonsHelper.kt
@@ -0,0 +1,7 @@
+package com.example.evlab2_pdm_00084417
+
+import android.view.View
+
+interface ButtonsHelper {
+    fun onPrevClickListener()
+}
